27b04be56df9d9f353ae266f993517b309883517
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-25 10:08:00 -0400

    Project Start

---
+++ IfThenElse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfThenElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
